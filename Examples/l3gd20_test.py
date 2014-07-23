a8fd1ff41b2e741138488c07aa9f47fd02971411
Peter Szamosi <qqtyin@users.noreply.github.com>
2014-07-23 10:33:01 +0200

    Update l3gd20_test.py
    Commented and fixed python errors.

--- Examples/l3gd20_test.py
+++ Examples/l3gd20_test.py
@@ -1,4 +1,15 @@
+#!/usr/bin/python
+
 import l3gd20, time
+
+# ===========================================================
+# Example for the Gyro click
+# After the installation of py-spidev and all the python tools
+# This example will just print the X Y Z calues of the Gyro click
+#
+# NOTE: This progam has an endless loop.
+# ===========================================================
+
 
 acc = l3gd20.L3GD20(1, 0x6B)
 
@@ -8,4 +19,4 @@ while(1):
 	Z = acc.readRawZ()
 	print('X:' + str(X) + ' Y:' + str(Y) + ' Z:' + str(Z))
 	#print(X, Y, Z)
-	time.sleep(0.2)
+	time.sleep(0.25)
