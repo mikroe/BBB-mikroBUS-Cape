4e100c86bb2a63e0c5872d43c198a73163cdf62f
Peter Szamosi <szamosi.peter@gmail.com>
2014-07-30 16:02:48 +0200

    expand example update

--- Examples/expand_read.py
+++ Examples/expand_read.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+
+
 import spidev
 import time
 
--- Examples/expand_write.py
+++ Examples/expand_write.py
@@ -1,11 +1,16 @@
+#!/usr/bin/python
+
+
 import spidev
 import time
 
 spi = spidev.SpiDev()
 spi.open(1,0)  # /dev/spidev1.0
 
-#Initialise
-# Set porta to be output 
-spi.writebytes(0,[0x00])
 
-Expander_Set_PullUpsPortB(0,0xFF);     // Set pull-ups to all of the Expander's PORTB pins
+#Turns ports on and off in an infinite loop
+while True:
+	spi.xfer([0,0,0])		#Turns all ports low
+	time.sleep(10)
+	spi.xfer([1,255,254])	#Turns all ports high
+	time.sleep(10)
