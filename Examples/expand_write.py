80ec3a84ba853526a6366333356f177ca7a14191
Peter Szamosi <szamosi.peter@gmail.com>
2014-07-30 16:09:26 +0200

    update

--- Examples/expand_write.py
+++ Examples/expand_write.py
@@ -11,6 +11,7 @@ spi.open(1,0)  # /dev/spidev1.0
 #Turns ports on and off in an infinite loop
 while True:
 	spi.xfer([0,0,0])		#Turns all ports low
-	time.sleep(10)
+	time.sleep(10)			#time of 10 makes it possible to measure the change
+	time.sleep(10)			#time of 10 makes it possible to measure the change
 	spi.xfer([1,255,254])	#Turns all ports high
 	time.sleep(10)
