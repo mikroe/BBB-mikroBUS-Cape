839ca071f16766c4b00ad54a4a1cf52621139c1d
Peter Szamosi <szamosi.peter@gmail.com>
2014-07-30 15:06:19 +0200

    read update

--- Examples/expand_read.py
+++ Examples/expand_read.py
@@ -6,9 +6,9 @@ read = 0
 spi = spidev.SpiDev()
 spi.open(1,0)  # /dev/spidev1.0
 
-#Check the states
+#Check the states for 16 bits
 def read():
-		print spi.readbytes(20)
+		print spi.readbytes(16)
 		
 	
 read()
